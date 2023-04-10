96f6d0934d75ac2612150c6e193d571f46a6eaf7
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-04-10 08:48:31 -0500

    Se crea el authLayout con material UI para reutilizarlos en el LoginPage y el RegisterPage

---
+++ src/auth/layout/AuthLayout.tsx
@@ -0,0 +1,36 @@
+import { Grid, Typography } from "@mui/material"
+
+interface AuthLayoutProps {
+    // Los componentes hijos son de tipo JSX.Element
+    children: JSX.Element | JSX.Element[],
+    title: string
+}
+
+// El AuthLayout va a tener componentes hijos y el titulo
+export const AuthLayout = ({ children, title }: AuthLayoutProps) => {
+    return (
+        // Grid container se puede ver como un div
+        <Grid container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            // sx permite personalizar estilos adicionales y acceder a los estilos del tema que tengan definido, ejemplo purpleTheme
+            sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+        >
+            <Grid item
+                className="box-shadow"
+                xs={3}
+                sx={{
+                    width: { sm: 450 },
+                    backgroundColor: 'white',
+                    padding: 3,
+                    borderRadius: 2
+                }}
+            >
+                <Typography variant="h5" sx={{ mb: 1 }}>{title}</Typography>
+                {children}
+            </Grid>
+        </Grid>
+    )
+}
