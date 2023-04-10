e74a951b43b307164a65b707c088408f1c0af080
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-04-10 08:55:13 -0500

    Se personaliza el RegisterPage teniendo en cuenta el AuthLayout

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,6 +1,61 @@
+import { Link as RouterLink } from "react-router-dom"
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from "../layout/AuthLayout"
 
 export const RegisterPage = () => {
     return (
-        <div>RegisterPage</div>
+        <AuthLayout title="Crear cuenta">
+            <form>
+                <Grid container>
+                    {/* xs indica el tamano a tomar en relación con el div o grid */}
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Nombre completo"
+                            type="text"
+                            placeholder="Nombre Ccompleto"
+                            fullWidth>
+                        </TextField>
+                    </Grid>
+
+                    {/* xs indica el tamano a tomar en relación con el div o grid */}
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Correo"
+                            type="email"
+                            placeholder="correo@google.com"
+                            fullWidth>
+                        </TextField>
+                    </Grid>
+
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Contrasena"
+                            type="password"
+                            placeholder="contrasena"
+                            fullWidth>
+                        </TextField>
+                    </Grid>
+
+                    <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={12}>
+                            <Button variant="contained" fullWidth>
+                                Crear cuenta
+                            </Button>
+                        </Grid>
+
+                    </Grid>
+                    <Grid container direction="row" justifyContent="end">
+                        <Typography mr={1}>Ya tienes cuenta ?</Typography>
+                        {/* El primer Link es el material ui, el cual permite trabajar con estilos y demás */}
+                        {/* El RouterLink es un alias de Link de react-router-dom el cual si permite redireccionar a otras páginas */}
+                        <Link component={RouterLink} color="inherit" to="/auth/login">
+                            ingresar
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
+
     )
 }
