1e936ab9ec77f8575232558beadf8d0e59d5754e
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-04-11 10:13:04 -0500

    Creacion de un botón flotante en el JournalPage medianto Material UI

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,5 +1,8 @@
-import { JournalLayout } from "../layout/JournalLayout"
-import { NoteView, NothingSelectedView } from "../views"
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
     return (
@@ -9,7 +12,21 @@ export const JournalPage = () => {
             <NothingSelectedView />
 
             {/* NoteView */}
-            <NoteView />
+            {/* s<NoteView /> */}
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
 
         </JournalLayout>
     )
