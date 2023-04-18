2508a5469b9bd93a31075761300df476a6750198
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-04-18 08:59:39 -0500

    Instalación de react-toolkit, creación del store

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import './styles.css'
 import { JournalApp } from './JournalApp'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    {/* Provee el store a toso los componentes hijos */}
+    <Provider store={store}> 
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
