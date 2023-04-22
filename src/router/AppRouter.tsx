aaf8a98d5516e948ce7b5c5311e3db59ab922e22
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-04-22 14:01:59 -0500

    Creation a custom hook useCheckAuth to check of authentication status

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,61 +1,16 @@
-import { useEffect } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { onAuthStateChanged } from "firebase/auth"
-import { FirebaseAuth } from "../firebase/config"
 
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
 
 import { CheckingAuth } from "../ui"
-import { RootState, useAppDispatch } from "../store"
-import { login, logout } from "../store/auth"
-import { Auth, AuthAction } from "../interfaces"
+import { useCheckAuth } from "../hooks"
 
 
 export const AppRouter = () => {
 
-    // el hook useSelector de react-redux permite leer datos del store
-    const { status } = useSelector((state: RootState) => state.auth);
-
-    // con useDispath puedo ejecutar cualquier acción, ya sea desde un thunks, o directamente desde el reducer del store
-    const dispatch = useAppDispatch();
-
-    // cada que vez que se recarge la aplicación
-    useEffect(() => {
-        // Adds an observer for changes to the user's sign-in state.
-        // Obtiene el usuario actual
-        onAuthStateChanged(FirebaseAuth, async (user) => {
-
-            console.log(user)
-            if (!user) {
-                let authAction: AuthAction = {
-                    auth: null!,
-                    errorMessage: '',
-                    status: 'not-authenticated',
-                    ok: false
-                }
-                return dispatch(logout(authAction));
-            }
-
-            const { displayName, email, photoURL, uid } = user;
-            let auth: Auth = {
-                displayName: displayName!,
-                email: email!,
-                photoURL: photoURL!,
-                uid: uid!
-            }
-            let authAction: AuthAction = {
-                auth: auth,
-                errorMessage: '',
-                status: 'authenticated',
-                ok: true
-            }
-            dispatch(login(authAction));
-        })
-
-    }, [])
-
+    // useCheckAuth is a custom hook, to check of authenticaction status
+    const { status } = useCheckAuth();
 
     if (status === 'checking') {
         return <CheckingAuth />
